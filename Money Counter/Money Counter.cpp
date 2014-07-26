9b7ccfa902d869d032693d6b07c5fb753c022265
Ben Choi <bjchoi514@gmail.com>
2014-07-26 12:08:12 -0700

    1

---
+++ Money Counter/Money Counter.cpp
@@ -0,0 +1,25 @@
+// Money Counter.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	double number;
+	string type;
+	while (cin >> number >> type){
+		string dollar = number;
+		string quarter = number * 4;
+		string dime = number * 10;
+		if (number > 1)
+
+		else if (number == 1)
+	}
+	return 0;
+}
+
